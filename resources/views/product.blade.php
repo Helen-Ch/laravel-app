abc75215993816c13eb3b55f8a1b606860305ae2
4ernovaolena <4ernovaolena@gmail.com>
2021-05-19 10:30:00 +0300

    lesson 23

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -13,28 +13,32 @@
     </div>
     <p>{{ $product->description }}</p>
     @if($product->isAvailable())
-        <a class="btn btn-success" href="{{ route('basket-add', $product->code) }}">Добавить в корзину</a>
+        <form action="{{ route('basket-add', $product) }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
+        </form>
+{{--        <a class="btn btn-success" href="{{ route('basket-add', $product->code) }}">Добавить в корзину</a>--}}
     @else
         <button type="button" class="btn btn-info" role="button" disabled>Нет в наличии</button>
     @endif
-    <div  style="max-width: 50%; margin: 0 auto;">
+    <div style="max-width: 50%; margin: 0 auto;">
         <p class="text-left">Оставьте свой отзыв:</p>
         <form action="{{ route('reviews.store', $product) }}" method="post">
             @csrf
-                <div class="form-group">
-                    @error('author')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="text" name="author" class="form-control"placeholder="Ваше имя"
-                            value="{{ old('code', isset($review) ? $review->author : null) }}">
-                </div>
+            <div class="form-group">
+                @error('author')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <input type="text" name="author" class="form-control" placeholder="Ваше имя"
+                       value="{{ old('code', isset($review) ? $review->author : null) }}">
+            </div>
 
-                <div class="form-group">
-                    @error('text')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <textarea name="text" class="form-control" rows="3"></textarea>
-                </div>
+            <div class="form-group">
+                @error('text')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <textarea name="text" class="form-control" rows="3"></textarea>
+            </div>
             <button class="btn btn-success">Отправить</button>
         </form>
     </div>
